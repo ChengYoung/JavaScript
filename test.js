2ad6144dcadd70d6a8ec2bb81899df41d37375c7
止于此始于斯 <qiancheng123456@live.com>
2018-01-29 23:38:23 +0800

    Jan 29th

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-alert("hello!");
+/* var message;
+var age = null;
+alert(typeof message);
+alert(typeof null);
+alert(message == age); */
+// var num = 10.0;
+// alert(parseInt("0xAF",16));
+/* var num = 10;
+alert(num.toString());
+alert(num.toString(2));
+alert(num.toString(8));
+alert(num.toString(16)); */
+var num = NaN;
+alert(num.toString());
